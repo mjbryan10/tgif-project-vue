a77fbbbf92d8ef2d0169f1866d88af892fb18242
mjbryan <mjbyran10@gmail.com>
2020-02-04 15:20:47 +0100

    Filters now functional, documentation added to JS, helper note added to html

--- resources/js/vue-table.js
+++ resources/js/vue-table.js
@@ -30,7 +30,7 @@ var app = new Vue({
                 congress = 'house';
             } else {
                 congress = 'senate';
-            }
+            } //Changes the data API depending on page name
             let url = `https://api.propublica.org/congress/v1/113/${congress}/members.json`;
             console.log("TCL: getData -> url", url)
 
@@ -46,13 +46,6 @@ var app = new Vue({
             console.log("TCL: getData -> this.members", this.members)
             this.loading = false;
 
-
-            //TODO: Move this to computed
-            for (const member of this.members) {
-                this.member_states.push(member.state)
-            }
-
-            //??? -- why is this not updating?
             this.updatePct10('attendance', 'missed_votes_pct');
             this.updatePct10('loyal', 'votes_with_party_pct');
         },
@@ -65,7 +58,7 @@ var app = new Vue({
             }
             return memberArr
         },
-        averageWith(array) {
+        averageWith(array) { //Works out average of vote %
             let sum = 0
             for (const i of array) {
                 sum += i.votes_with_party_pct
@@ -75,7 +68,7 @@ var app = new Vue({
             }
             return (sum / array.length).toFixed(2)
         },
-        sortMembers(property, reverse = false) {
+        sortMembers(property, reverse = false) { //Sorts via property (can also reverse the sort)
             let new_dataset = this.members.slice();
             function compare(a, b) {
                 if (reverse === true) {
@@ -85,27 +78,34 @@ var app = new Vue({
             }
             return new_dataset.sort(compare);
         },
-        pct10(array, property) {
+        pct10(array, property) { //Finds 10% of array and adds to length if last has duplicates
             let end = Math.floor((array.length / 100) * 10);
             while (array[end][property] === array[end + 1][property]) {
                 end++
             }
             return end
         },
-        updatePct10(obj, val){
+        updatePct10(obj, val) { //takes the object parent and value to populate tables
             let array = this.sortMembers(val)
             let arrayR = this.sortMembers(val, true)
             this[obj].least = array.slice(0, this.pct10(array, val))
             this[obj].most = arrayR.slice(0, this.pct10(arrayR, val))
 
+        },
+        resetTable() { //Resets the main tables (data pages)
+            this.filter.state = "All";
+            this.filter.republican = false;
+            this.filter.democrat = false;
+            this.filter.independent = false;
         }
     },
     computed: {
-        uniqueStateList() {
-            this.member_states = Array.from(new Set(this.member_states)).sort();
-            return this.member_states.sort();
+        uniqueStateList() { //Removes duplicates
+            uniqueList = Array.from(new Set(this.stateList)).sort(); //member_states
+            // uniqueList = uniqueArray(this.stateList).sort(); //member_states
+            return uniqueList;
         },
-        parties() {
+        parties() { //Creates list of checkbox selected parties
             let partyList = [];
             if (this.filter.republican) { partyList.push('R') };
             if (this.filter.democrat) { partyList.push('D') };
@@ -125,13 +125,40 @@ var app = new Vue({
             }
             return array
         },
-        // member_states(){
-        //     let array = [];
-        //     for (const member of this.filteredMembers) {
-        //         array.push(member.state)
-        //     }
-        //     return array
-        // }
+        stateList() { //List of states, affected by checkbox filter
+            let list = [];
+            for (const member of this.members) {
+                if (this.parties.includes(member.party) || this.parties.length === 0) {
+                    list.push(member.state)
+                }
+            }
+            return list
+        },
+        possibleParties() { //Finds possible parties with state selected
+            let partyArr = [];
+            for (const member of this.members) {
+                if (this.filter.state === "All" || member.state === this.filter.state) { //this.parties.includes(member.party)
+                    if (member.party === "R") {
+                        partyArr.push("Republican");
+                    } if (member.party === "D") {
+                        partyArr.push("Democrat");
+                    } if (member.party === "I") {
+                        partyArr.push("Independent");
+                    }
+                }
+            }
+            partyArr = Array.from(new Set(partyArr)) // Remove dupes
+            return partyArr;
+        },
+        notPossibleParties(){ //Uses possible parties to find the impossible 
+            let parties = ['Republican', 'Democrat', 'Independent']
+            parties = parties.filter(val => !(this.possibleParties.includes(val)))
+            return parties
+        },
+        partyText() { //Produces text for UX note
+            let pluralText = this.notPossibleParties.map(x => x += "s")
+            return pluralText.join(' or ').toString();
+        }
     }
 
 })
